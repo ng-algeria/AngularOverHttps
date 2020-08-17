4947c6fc1855768e72ce762361dc8271c145fe02
farouk <farouk@192.168.1.11>
2020-08-17 16:29:11 +0100

    inital commit for 02-CreateAngularTemplate

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,44 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+import { HomeComponent } from './home/home.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
 
 
-const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'signin',
+    component: SigninComponent
+  },
+
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+
+  {
+    path: 'experiences',
+    component: ExperiencesComponent
+  },
+  
+
+  {
+    path: '**',
+    redirectTo: '/experiences',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    redirectTo: '/experiences',
+    pathMatch: 'full'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+import { HomeComponent } from './home/home.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    HomeComponent,
+    ExperiencesComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
